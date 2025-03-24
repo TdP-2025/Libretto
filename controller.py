68f5feacef3fcc0025c0229a2327beb254558069
dalsassofilippo <s311881@studenti.polito.it>
2025-03-24 11:20:46 +0100

    INTERFACCIA GRAFICA MVC

--- controller.py
+++ controller.py
@@ -1,16 +1,72 @@
+from networkx.algorithms.bipartite.basic import color
+
+from scuola import Student
+from testLibretto import myLib
 from view import View
+from voto.voto import Libretto, Voto
+import flet as ft
 
 # PRENDE LA VIEW E LA ISTANZIA
 class Controller:
     def __init__(self,v: View):
         self._view=v
+        self._student=Student(nome="Harry", cognome="Potter", eta=11, capelli="castani", occhi="azzurri", casa="Grifondoro",
+                animale="civetta", incantesimo="Expecto Patronum")
+        self._model=Libretto(self._student,[])
+        self._fillLibretto()
 
-    def handleAdd(self,e):
-        strIn=self._view._txtIn.value
-        if strIn=="":
-            self._view._txtOut.value="ERRORE: campo vuoto"
+    def handleAggiungi(self,e):
+        nome=self._view._txtNome.value
+        if nome=="":
+            self._view._txtOut.controls.append(
+                ft.Text("Attenzione. Il campo nome non può essere vuoto", color="red")
+            )
             self._view._page.update()
             return
-        self._view._txtOut.value=strIn
-        self._view._txtIn.value=""
+        punti=self._view._ddVoto.value
+        if punti is None:
+            self._view._txtOut.controls.append(
+                ft.Text("Attenzione. Selezionare un voto", color="red")
+            )
+            self._view._page.update()
+            return
+
+        data=self._view._dp.value
+        if data is None:
+            self._view._txtOut.controls.append(
+                ft.Text("Attenzione. Selezionare una data", color="red")
+            )
+            self._view._page.update()
+            return
+
+        if punti=="30L":
+            self._model.append(Voto(nome,30,f"{data.year}-{data.month}-{data.day}",True))
+        else:
+            self._model.append(Voto(nome, int(punti), f"{data.year}-{data.month}-{data.day}", False))
+
+        self._view._txtOut.controls.append(
+            ft.Text("Voto aggiunto correttamente",color="green")
+        )
+
+
+
+    def handleStampa(self, e):
+        self._view._txtOut.controls.append(
+            ft.Text(str(self._model))
+        )
         self._view._page.update()
+
+    def getStudent(self):
+        """
+        Restituisce informazioni dello studente, usando il __str__ dello studente
+        :return:
+        """
+        return str(self._student)
+
+    def _fillLibretto(self):
+        v1 = Voto("Trasfigurazione", 24, "2022-02-13", False)
+        v2 = Voto("Pozioni", 27, "2022-02-17", True)
+
+        self._model.voti.append(v1)
+        self._model.voti.append(v2)
+        self._model.voti.append(Voto("Difesa contro le arti oscure", 22, "2022-04-13", False))
--- view.py
+++ view.py
@@ -1,9 +1,17 @@
+import datetime
+
 import flet as ft
+from networkx.algorithms.bipartite.basic import color
+
 
 # CREA UN CONTROLLER VUOTO E HA UN METODO SET CONTROLLER
 # PRENDE UNA PAGINA DA FUORI LA IMPOSTA COME INTERNA
 class View:
     def __init__(self,page:ft.Page):
+        self._ddVoto = None
+        self._txtNome = None
+        self._student = None
+        self._titolo = None
         self._txtOut = None
         self._btnIn = None
         self._txtIn = None
@@ -16,12 +24,44 @@ class View:
         tutto il resto va nel controller.
         :return:
         """
-        self._txtIn=ft.TextField(label="Inserisci nome") # self._ PER ESSERE ISTANZIABILI NELLA CLASSE CONTROLLER
-        self._btnIn=ft.ElevatedButton("Aggiungi", on_click=self._controller.handleAdd)
-        row=ft.Row(controls=[self._txtIn,self._btnIn])
-        self._txtOut=ft.Text("")
+        self._page.bgcolor="white"
+        self._titolo=ft.Text(value="Libretto Voti", color="red", size=24)
+        self._student=ft.Text(value=self._controller.getStudent(),color="brown")
+        row1=ft.Row([self._titolo],alignment=ft.MainAxisAlignment.CENTER)
+        row2=ft.Row([self._student],alignment=ft.MainAxisAlignment.END)
 
-        self._page.add(row,self._txtOut)
+        # self._txtIn=ft.TextField(label="Inserisci nome") # self._ PER ESSERE ISTANZIABILI NELLA CLASSE CONTROLLER
+        # self._btnIn=ft.ElevatedButton("Aggiungi", on_click=self._controller.handleAdd)
+
+        #RIGA DEI CONTROLLI
+        self._txtNome=ft.TextField(label="Nome esame", hint_text="Inserisci il nome dell'esame", width=300)
+        self._ddVoto=ft.Dropdown(label="Voto",width=120)
+
+        self._fillDDVoto()
+
+        self._dp=ft.DatePicker(
+            first_date=datetime.datetime(2022,1,1),
+            last_date=datetime.datetime(2026,12,31),
+            on_change= lambda e:print(f"Giorno selezionato: {self._dp.value}"),
+            on_dismiss= lambda e:print("Data non selezionata")
+        )
+        self._btnCal=ft.ElevatedButton("Segli data",
+                                       icon=ft.Icons.CALENDAR_MONTH,
+                                       on_click=lambda e:self._page.open(self._dp))
+
+        self._btnAdd=ft.ElevatedButton("Aggiungi",on_click=self._controller.handleAggiungi)
+        self._btnPrint=ft.ElevatedButton("Stampa", on_click=self._controller.handleStampa)
+
+        row3=ft.Row(controls=[self._txtNome,self._ddVoto, self._btnCal, self._btnAdd,self._btnPrint], alignment=ft.MainAxisAlignment.CENTER)
+
+        self._txtOut=ft.ListView(expand=True)
+        self._page.add(row1,row2,row3,self._txtOut)
+
 
     def setController(self, c):
         self._controller=c
+
+    def _fillDDVoto(self):
+        for i in range(18,30):
+            self._ddVoto.options.append(ft.dropdown.Option(str(i)))
+        self._ddVoto.options.append(ft.dropdown.Option("30L"))
